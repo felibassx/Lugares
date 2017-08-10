9543ce2ea55065adead5617a6b55a25f9b182c4b
felibasscl <felibass@gmail.com>
2017-08-09 21:20:54 -0400

    se añaden funcionalidades

---
+++ Lugares/AddPlaceViewController.swift
@@ -0,0 +1,72 @@
+//
+//  AddPlaceViewController.swift
+//  Lugares
+//
+//  Created by Felipe Hernandez on 8/9/17.
+//  Copyright © 2017 kafecode. All rights reserved.
+//
+
+import UIKit
+
+class AddPlaceViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0{
+            //creo un objeto uipickerimage
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                let imagePicker = UIImagePickerController()
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.delegate = self
+                
+                self.present(imagePicker, animated: true)
+                
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        self.imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.imageView.contentMode = .scaleAspectFill
+        self.imageView.clipsToBounds = true
+        
+        //Autolayaout
+        let leadingConstraint = NSLayoutConstraint(item: self.imageView, attribute: .leading, relatedBy: .equal, toItem: self.imageView.superview, attribute: .leading, multiplier: 1, constant: 0)
+        
+        leadingConstraint.isActive = true
+        
+        let trailingConstraint = NSLayoutConstraint(item: self.imageView, attribute: .trailing, relatedBy: .equal, toItem: self.imageView.superview, attribute: .trailing, multiplier: 1, constant: 0)
+        
+        trailingConstraint.isActive = true
+        
+        let topConstraint = NSLayoutConstraint(item: self.imageView, attribute: .top, relatedBy: .equal, toItem: self.imageView.superview, attribute: .top, multiplier: 1, constant: 0)
+        
+        topConstraint.isActive = true
+        
+        let bottomConstraint = NSLayoutConstraint(item: self.imageView, attribute: .bottom, relatedBy: .equal, toItem: self.imageView.superview, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        bottomConstraint.isActive = true
+        
+        
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+}
